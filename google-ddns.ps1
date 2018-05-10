da950b3bbb328990821b514b2d7064659b3aa007
Matt Perry <touche112@gmail.com>
2018-05-10 12:55:00 -0400

    Initial commit

---
+++ google-ddns.ps1
@@ -0,0 +1,19 @@
+﻿$user="un"
+$pass="pw"
+$hostname="subdomain.example.com"
+
+##dont change below this line##
+
+$pair="$($user):$($Pass)"
+
+$encodedcreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicauthvalue = "Basic $encodedcreds"
+
+$Headers = @{
+    Authorization = $basicauthvalue
+    }
+
+$params = @{hostname=$hostname}
+
+Invoke-WebRequest -Uri https://domains.google.com/nic/update -Method POST -Body $params -Headers $Headers -UseBasicParsing
